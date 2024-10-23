9f6a0ccddb146629b57d07564c43f765b183ac4c
MMorlanes <157699467+MMorlanes@users.noreply.github.com>
2024-10-23 11:34:05 +0200

    23.10.24

--- PaginaEjemplo/js/Usuarios.js
+++ PaginaEjemplo/js/Usuarios.js
@@ -1,11 +1,10 @@
 function guardarUsuario(){
-
     console.log('guardando');
 
     let opciones={ method: "GET",};
-    let parametros = "controlador=Usuarios&metodo=guardarUsuario";
+    let parametros ="controlador=Usuarios&metodo=guardarUsuario";
     parametros+= '&'+new URLSearchParams(
-                    new FormData(document.getElementById('formularioEdicion'))).toString();
+                    new FormData(document.getElementById('formularioAñadirUsuario'))).toString();
     fetch("C_Frontal.php?"+parametros,opciones)
         .then(res=>{
             if(res.ok){
@@ -16,12 +15,12 @@ function guardarUsuario(){
         .then(resultado=>{
             if(resultado.correcto=='S'){
                 document.getElementById('capaEditarCrear').innerHTML=resultado.msj;
-            }else{
+            }else{ //con error
                 document.getElementById('msjError').innerHTML=resultado.msj;
             }
             //document.getElementById(destino).innerHTML=vista;
         })
         .catch(err=>{
-            console.error("Error al guardar", err.message);
+            console.error("Error al guardar el usuario", err.message);
         })
 }
--- PaginaEjemplo/vistas/Usuarios/V_Usuarios_NuevoEditar.php
+++ PaginaEjemplo/vistas/Usuarios/V_Usuarios_NuevoEditar.php
@@ -3,12 +3,12 @@
     <form id="formularioAñadirUsuario" name="formularioAñadirUsuario">
         <div class="row">
             <div class="form-group col-md-6 col-sm-12">
-                <label for="apellido1">Apellido 1:</label>
-                <input type="text" id="apellido1" name="apellido1" class="form-control" placeholder="Apellido 1" required />
+                <label for="apellido_1">Apellido 1:</label>
+                <input type="text" id="apellido_1" name="apellido_1" class="form-control" placeholder="Apellido 1" required />
             </div>
             <div class="form-group col-md-6 col-sm-12">
-                <label for="apellido2">Apellido 2:</label>
-                <input type="text" id="apellido2" name="apellido2" class="form-control" placeholder="Apellido 2" />
+                <label for="apellido_2">Apellido 2:</label>
+                <input type="text" id="apellido_2" name="apellido_2" class="form-control" placeholder="Apellido 2" />
             </div>
         </div>
 
@@ -17,36 +17,32 @@
                 <label for="nombre">Nombre:</label>
                 <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre" required />
             </div>
-     
             <div class="form-group col-md-6 col-sm-12">
-    <label for="sexo">Sexo:</label>
-    <div id="sexo" name="sexo" class="d-flex align-items-center" style="border:none; padding-left: 0;">
-        <div class="form-check me-3">
-            <input type="radio" id="masculino" name="sexo" value="M" class="form-check-input" required>
-            <label for="masculino" class="form-check-label">Masculino.</label>
-        </div>
-        <div class="form-check me-3">
-            <input type="radio" id="femenino" name="sexo" value="F" class="form-check-input">
-            <label for="femenino" class="form-check-label">Femenino.</label>
-        </div>
-        <div class="form-check me-3">
-            <input type="radio" id="otro" name="sexo" value="O" class="form-check-input">
-            <label for="otro" class="form-check-label">Otr@/e.</label>
-        </div>
-        <div class="form-check">
-            <input type="radio" id="no_decir" name="sexo" value="N" class="form-check-input">
-            <label for="no_decir" class="form-check-label">Prefiero no decirlo, soy un paquete.</label>
-        </div>
-    </div>
-</div>
-
-
+                <label>Sexo:</label>
+                <div>
+                    <div class="form-check">
+                        <input type="radio" id="sexoM" name="sexo" value="H" class="form-check-input" required />
+                        <label for="sexoM" class="form-check-label">Hombre</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" id="sexoF" name="sexo" value="M" class="form-check-input" />
+                        <label for="sexoF" class="form-check-label">Mujer</label>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="row">
             <div class="form-group col-md-6 col-sm-12">
                 <label for="fechaAlta">Fecha de Alta:</label>
                 <input type="date" id="fechaAlta" name="fechaAlta" class="form-control" required />
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-md-6 col-sm-12">
+                <label for="login">Login:</label>
+                <input type="text" id="login" name="login" class="form-control" placeholder="Login" required />
             </div>
             <div class="form-group col-md-6 col-sm-12">
                 <label for="mail">Email:</label>
@@ -57,40 +53,42 @@
         <div class="row">
             <div class="form-group col-md-6 col-sm-12">
                 <label for="movil">Móvil:</label>
-                <input type="tel" id="movil" name="movil" class="form-control" placeholder="Móvil" />
+                <input type="tel" id="movil" name="movil" class="form-control" placeholder="Móvil" required />
             </div>
-            <div class="form-group col-md-6 col-sm-12">
-                <label for="login">Login:</label>
-                <input type="text" id="login" name="login" class="form-control" placeholder="Login" required />
-            </div>
-        </div>
-
-        <div class="row">
             <div class="form-group col-md-6 col-sm-12">
                 <label for="pass">Contraseña:</label>
                 <input type="password" id="pass" name="pass" class="form-control" placeholder="Contraseña" required />
             </div>
+
+            
+        </div>
+
+        <div class="row">
             <div class="form-group col-md-6 col-sm-12">
-                <label for="activo">¿Activo?</label>
-                <select id="activo" name="activo" class="form-control">
-                    <option value="S">Sí</option>
-                    <option value="N">No</option>
-                </select>
+                <label>¿Activo?</label>
+                <div>
+                    <div class="form-check">
+                        <input type="radio" id="activo" name="activo" value="S" class="form-check-input" />
+                        <label for="activo" class="form-check-label">Sí</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" id="activo" name="activo" value="N" class="form-check-input" />
+                        <label for="activo" class="form-check-label">No</label>
+                    </div>
+                </div>
             </div>
         </div>
 
         <div class="row">
-    <div class="col-lg-12">
-        <button type="button" class="btn btn-outline-primary" 
-        onclick="buscar('Usuarios', 'getVistaListadoUsuarios'
-        , 'formularioBuscar', 'capaResultadosBusqueda')">Guardar</button>
-
-        <button type="button" onclick="document.getElementById('capaEditarCrear').innerHTML='';"
-         class="btn btn-outline-secundary" 
-        >Cancelar</button>
-        <span id="msjError" name="msjError" style="color:blue;"></span>
-    </div>
-</div>
+            <div class="col-lg-12">
+                <button type="button" class="btn btn-primary" onclick="guardarUsuario();">
+                    Guardar
+                </button>
+                <button type="button" class="btn btn-secondary" onclick="document.getElementById('capaEditarCrear').innerHTML = '';">
+                    Cancelar
+                </button>
+                <span id="msjError" name="msjError" style="color:blue;"></span>
+            </div>
+        </div>
     </form>
-    <div class="container-fluid" id="capaResultadoAñadirUsuario"></div>
 </div>
